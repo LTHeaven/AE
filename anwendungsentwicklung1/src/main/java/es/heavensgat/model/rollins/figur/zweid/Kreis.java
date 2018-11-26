86c5ccf92a43df99c22fc22626e5bb66e577e2f7
Philip Maaß <PhilipMaasz@aol.com>
2018-11-26 19:38:50 +0100

    adds annotations for class Kreis

--- anwendungsentwicklung1/src/main/java/es/heavensgat/model/rollins/figur/zweid/Kreis.java
+++ anwendungsentwicklung1/src/main/java/es/heavensgat/model/rollins/figur/zweid/Kreis.java
@@ -2,45 +2,80 @@ package es.heavensgat.model.rollins.figur.zweid;
 
 import es.heavensgat.model.bbender.figur.Figur2D;
 
-public class Kreis extends Figur2D {
+/**
+ * class Kreis
+ */
+public class Kreis extends Figur2D
+{
 	private double radius;
-	
+
+	/**
+	 * Kreis constructor
+	 *
+	 * @param r
+	 */
 	public Kreis(double r)
 	{
+
 		radius = r;
 	}
+
+	/**
+	 * Kreis constructor
+	 */
 	public Kreis()
 	{
-		radius=1;
+		radius = 1;
 	}
-	
+
+	/**
+	 * @param radius
+	 *
+	 * @return
+	 */
 	public boolean setRadius(double radius)
 	{
-		boolean zulaessig=false;
+		boolean zulaessig = false;
+
 		if(radius > 0)
 		{
-			zulaessig=true;
+			zulaessig = true;
 			this.radius = radius;
-		}
-		else
-			if(radius < 0)
+		}else {
+			if (radius < 0)
+			{
 				this.radius = -radius;
-			else 
-				this.radius= 1;
+			} else {
+				this.radius = 1;
+			}
+		}
+
 		return zulaessig; 
 	}
+
+	/**
+	 * @return double
+	 */
 	public double getRadius()
 	{
 		return this.radius;
 	}
+
+	/**
+	 * @return double
+	 */
 	public double flaeche()
 	{
-		double flaeche= this.radius * this.radius * Math.PI;
+		double flaeche = this.radius * this.radius * Math.PI;
+
 		return flaeche;
 	}
+
+	/**
+	 * @return double
+	 */
 	public double umfang()
 	{
 		return Math.PI * 2 * radius;
 	}
-
 }
