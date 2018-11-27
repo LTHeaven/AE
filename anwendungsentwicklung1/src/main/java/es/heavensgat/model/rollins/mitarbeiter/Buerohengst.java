b6d3e8626fa31f76e2dde8a12a0e6b9cef43cea3
Philip Maaß <PhilipMaasz@aol.com>
2018-11-27 09:01:04 +0100

    Buerohengst refactored to a more beautiful structure

--- anwendungsentwicklung1/src/main/java/es/heavensgat/model/rollins/mitarbeiter/Buerohengst.java
+++ anwendungsentwicklung1/src/main/java/es/heavensgat/model/rollins/mitarbeiter/Buerohengst.java
@@ -1,33 +1,80 @@
 package es.heavensgat.model.rollins.mitarbeiter;
 
-public class Buerohengst extends Mitarbeiter{
 
+/**
+ * class Buerohengst
+ */
+public class Buerohengst extends Mitarbeiter
+{
 	private double festgehalt;
 
-	public Buerohengst(Buerohengst original) {
+	/**
+	 * Buerohengst constructor
+	 *
+	 * @param original
+	 */
+	public Buerohengst(Buerohengst original)
+	{
 		super(original.getID(), original.getName());
 		festgehalt = original.getFestgehalt();
 	}
-	public Buerohengst(int id, String name, double festgehalt) {
+
+	/**
+	 * Buerohengst constructor
+	 *
+	 * @param id
+	 * @param name
+	 * @param festgehalt
+	 */
+	public Buerohengst(int id, String name, double festgehalt)
+	{
 		super(id, name);
 		setFestgehalt(festgehalt);
 	}
 
-	public double getFestgehalt() {
-		return festgehalt;
-	}
-	public double einkommen() {
+	/**
+	 * @return double
+	 */
+	public double getFestgehalt()
+	{
 		return festgehalt;
 	}
 
-	public void setFestgehalt(double festgehalt) {
-		if(festgehalt > 300) // Mindestlohn 300 Euro
+	/**
+	 * @return double
+	 */
+	public double einkommen()
+	{
+		return festgehalt;
+	}
+
+	/**
+	 * @param int festgehalt
+	 */
+	public void setFestgehalt(double festgehalt)
+	{
+		// Mindestlohn 300 Euro
+		if(festgehalt > 300)
+		{
 			this.festgehalt = festgehalt;
-		else this.festgehalt = 300;
+		} else{
+			this.festgehalt = 300;
+		}
 	}
-	protected void setID(int id){
+
+	/**
+	 * @param int id
+	 */
+	protected void setID(int id)
+	{
+		this.id = id;
 	}
-	public String toString(){
+
+	/**
+	 * @return string
+	 */
+	public String toString()
+	{
 		return super.toString() +"Buerohengst hat Einkommen: " + festgehalt;
 	}
 }
